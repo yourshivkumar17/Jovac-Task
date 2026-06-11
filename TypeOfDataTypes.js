3ddda27fecac8541a675553ad5ffd11a046d6c91
Shiv Kumar <yourshivkumar17@gmail.com>
2026-06-11 10:06:44 +0530

    Add TypeOfDataTypes.js

---
+++ TypeOfDataTypes.js
@@ -0,0 +1,15 @@
+let age = 19;
+let name = "Shiv";
+let isStudent = true;
+let x;
+let y = null;
+let arr = [1, 2, 3];
+let obj = {name: "Shiv"};
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof arr);
+console.log(typeof obj);
